9fe0e55cc1ad55d92161b4cdd8d23a4152177758
riccardovacirca <rvacirca23@gmail.com>
2026-02-26 14:08:03 +0000

    updated

--- modules/auth/java/auth/dto/ChangePasswordDTO.java
+++
@@ -1,3 +0,0 @@
-package com.example.auth.dto;
-
-public record ChangePasswordDTO(String currentPassword, String newPassword) {}
--- modules/auth/java/auth/dto/TwoFactorCredentialDTO.java
+++
@@ -1,3 +0,0 @@
-package com.example.auth.dto;
-
-public record TwoFactorCredentialDTO(String challengeToken, String pin) {}
