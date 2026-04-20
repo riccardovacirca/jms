a49aa2a29f9f1e671196e26ad2be5315b5d4e20a
riccardovacirca <rvacirca23@gmail.com>
2026-04-20 05:28:15 +0000

    updated

--- module/___crm/db/uninstall/V20260301_000001__crm_uninstall.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS jms_crm_campagna_liste;
-DROP TABLE IF EXISTS jms_crm_lista_contatti;
-DROP TABLE IF EXISTS jms_crm_turno;
-DROP TABLE IF EXISTS jms_crm_import_sessions;
-DROP TABLE IF EXISTS jms_crm_campagne;
-DROP TABLE IF EXISTS jms_crm_liste;
-DROP TABLE IF EXISTS jms_crm_contatti;
